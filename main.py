9fb10be3f8e3345f9ffc92afc0617cb8a82aad7b
Tannmay Khandelwal <94522751+tannmaycoding@users.noreply.github.com>
2024-09-29 13:00:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Time Related Operations")
+st.markdown('''This app is designed to perform various time related operations such as 
+- An alarm 
+- Time teller of different timezones
+- Time converter 
+- Stopwatch
+- Timer, all in one place.''')
